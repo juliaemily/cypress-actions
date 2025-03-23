fca121ac1d368089466f37d9055404acef5cdc69
Júlia Emily <83286441+juliaemily@users.noreply.github.com>
2025-03-23 18:06:04 -0300

    Update runner-firefox.js

--- runner-firefox.js
+++ runner-firefox.js
@@ -2,7 +2,7 @@ const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
 cypress.run({
-  browser: "firefox"
+  browser: 'firefox'
 })
 .then((results) => {
   const args = {
